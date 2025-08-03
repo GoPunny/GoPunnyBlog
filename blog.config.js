4d30ce2acabd8af694f72d8d0534fbd98ac0bf2e
Punny <2472207427@qq.com>
2025-08-03 21:02:16 +0800

    解决添加github联系方式失效问题

--- blog.config.js
+++ blog.config.js
@@ -24,8 +24,7 @@ const BLOG = {
 
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
-  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/GoPunny/', // 你的github个人主页 例如 https://github.com/tangly1024
-
+  
   // 其它复杂配置
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
   ...require('./conf/comment.config'), // 评论插件
